9d7b228cab73cedcf00420a42b3c4ca1f849e439
MarcellaJP <marcellawijngaarden@hotmail.com>
2022-02-03 12:51:21 +0100

    Add location detection in middleware

--- international/middleware.py
+++ international/middleware.py
@@ -1,9 +1,25 @@
+import datetime
 from django.utils.deprecation import MiddlewareMixin
 from django.utils import translation
 from django.conf import settings
-
+from django.utils import timezone 
 from .models import CountrySite
+from .views import get_country_data_from_request
 
+def is_crawler_request(request):
+    """
+    Very basic crawler detection using user agent
+    """
+
+    user_agent = request.META.get("HTTP_USER_AGENT")
+    if user_agent:
+        if "bot" in user_agent.lower():
+            return True 
+
+        if "spider" in user_agent.lower():
+            return True
+
+    return False
 
 class InternationalSiteMiddleware(MiddlewareMixin):
     """
@@ -29,5 +45,25 @@ class InternationalSiteMiddleware(MiddlewareMixin):
         if local != country_code:
             response.set_cookie("local", country_code)
             request.session["local"] = country_code
+
+        # Detect location if not know already
+        if not getattr(settings, "GEOIP_REDIRECT", False):
+            if not request.COOKIES.get("local_dc", ""):
+
+                # Skip location detection for known crawlers
+                if not is_crawler_request(request):
+                    detection = get_country_data_from_request(request)
+                    detected_country_code = {"GB": "UK"}.get(detection["country"], detection["country"])
+                else:
+                    # For crawlers use value of current country site
+                    detected_country_code = country_code
+
+                expires = timezone.now() + timezone.timedelta(days=2)
+                expires = datetime.datetime.strftime(expires, "%a, %d-%b-%Y %H:%M:%S GMT")
+
+                if not detected_country_code:
+                    detected_country_code = settings.DEFAULT_COUNTRY_CODE
+                    
+                response.set_cookie("local_dc", detected_country_code, expires=expires)
 
         return response
